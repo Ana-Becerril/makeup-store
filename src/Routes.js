a5a28323ad47d4e5b806a449c001efc86f6c4f02
Ana- Becerril <ana.becbel@gmail.com>
2021-09-07 19:08:11 -0500

    routing

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,26 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+import NavBar from './components/NavBar'
 import Home from './components/Home'
 import Shop from './components/Shop'
 import Contact from './components/Contact'
 import Cart from './components/Cart'
 
 const Routes = () => {
+
   return (
     <BrowserRouter>
+    <div className="App">
+      <NavBar/>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/shop" component={Shop} />
-        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/" component={Home}/>
+        <Route path="/shop" component={Shop}/>
+        <Route path="/contact" component={Contact}/>
         <Route exact path="/cart" component={Cart} />
-
       </Switch>
+    </div>
     </BrowserRouter>
   );
-};
+}
 
 export default Routes;
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,9 +1,36 @@
+import React from 'react';
+import styles from './style.module.css';
+import { NavLink } from 'react-router-dom';
+
 const NavBar = () => {
-    return (
-      <div>
-        <h1>Hello from NavBar</h1>
-      </div>
-    );
-  };
-  
-  export default NavBar;
+
+  // const history = useHistory();
+
+  // const goTo = (path) => {
+  //   if (path === "shop" ) {
+  //     history.push("/shop")
+  //   }
+  // }
+
+  return (
+
+    <div className={styles.navContainer} >
+        <div className={styles.bar}>
+            <div className={`${styles.home} ${styles.coolLink}`}>
+            <NavLink exact to="/">Home</NavLink>
+            </div>
+            <div className= {`${styles.shop} ${styles.coolLink}`}>
+            <NavLink to="/shop">Shop</NavLink>
+            </div>
+            <div className= {`${styles.contact} ${styles.coolLink}`}>
+            <NavLink to="/contact">Contact</NavLink>
+            </div>
+            <div className= {`${styles.cart} ${styles.coolLink}`}>
+            <NavLink to="/cart">Cart</NavLink>
+            </div>
+        </div>
+    </div>
+);
+} 
+
+export default NavBar;
