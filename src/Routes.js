4f30f6e945a2f8425af8eafdfe85fcca372d00cc
Ana- Becerril <ana.becbel@gmail.com>
2021-09-10 22:55:30 -0500

    working on home page

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route} from "react-router-dom";
+import NavBar from './components/NavBar'
 import Home from './components/Home'
 import Shop from './components/Shop'
 import Contact from './components/Contact'
@@ -11,6 +12,7 @@ const Routes = () => {
   return (
     <BrowserRouter>
     <div className={styles.main}>
+      <NavBar/>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/shop" component={Shop}/>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.container}>
-        <video autoplay muted loop className={styles.video}>
-        <source src="https://vod-progressive.akamaized.net/exp=1631329691~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4181%2F23%2F595906523%2F2804939859.mp4~hmac=c9f286afba5d2fd1aea511794b1c71197f74a61c58fd4266ce21baed3a71f915/vimeo-prod-skyfire-std-us/01/4181/23/595906523/2804939859.mp4?filename=pexels-thirdman-9401750.mp4"/>
+        <video autoPlay muted loop className={styles.video}>
+        <source src="https://vod-progressive.akamaized.net/exp=1631345764~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4181%2F23%2F595906523%2F2804939859.mp4~hmac=a31cd490e767698cea03620be1280699b6fde05d21bec49aebf4ca8359f22580/vimeo-prod-skyfire-std-us/01/4181/23/595906523/2804939859.mp4?filename=pexels-thirdman-9401750.mp4" type="video/mp4"/>
         </video>
         <div className={styles.txtContainer}>
         <h1>WE LOVE <span>MAKEUP</span> </h1>
