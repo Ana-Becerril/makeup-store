b95408b8ab04c5da882721c194ab201c7908ded9
Ana- Becerril <ana.becbel@gmail.com>
2021-09-14 14:03:52 -0500

    working on shop page

--- src/components/Shop/index.js
+++ src/components/Shop/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import styles from './style.module.css';
 import {useState, useEffect} from 'react';
 import axios from "axios";
+
+const client = axios.create({
+  baseURL: "http://makeup-api.herokuapp.com/api/v1/products.json" 
+});
+
 const Shop = () => {
 
-  const baseURL = "http://makeup-api.herokuapp.com/api/v1/products.json"
-
-  const [post, setPost] = useState(null);
-
-  useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
+  const [title, setTitle] = useState("")
+   const [products, setProducts] = useState(null);
+  
+  // useEffect(() => {
+  //   client.get("/product_type").then((response) => {
+  //     setProducts(response.data);
+  //   });
+  // }, []);
 
 
     return (
@@ -20,19 +24,21 @@ const Shop = () => {
         <div className={styles.leftContainer}>
           <h2>CATEGORIES</h2>
           <ul>
-            <li>Blush</li>
-            <li>Bronzer</li>
-            <li>Eyebrow</li>
-            <li>Eyeliner</li>
-            <li>Eyeshadow</li>
-            <li>Foundation</li>
-            <li>Lip liner</li>
-            <li>Lipstick</li>
-            <li>Mascara</li>
-            <li>Nail polish</li>
+            <li  onClick={() => setTitle("Blush")}>Blush</li>
+            <li  onClick={() => setTitle("Bronzer")}>Bronzer</li>
+            <li  onClick={() => setTitle("Eyebrow")}>Eyebrow</li>
+            <li  onClick={() => setTitle("Eyeliner")}>Eyeliner</li>
+            <li  onClick={() => setTitle("Eyeshadow")}>Eyeshadow</li>
+            <li  onClick={() => setTitle("Foundation")}>Foundation</li>
+            <li  onClick={() => setTitle("Lip liner")}>Lip liner</li>
+            <li  onClick={() => setTitle("Lipstick")}>Lipstick</li>
+            <li  onClick={() => setTitle("Mascara")}>Mascara</li>
+            <li  onClick={() => setTitle("Nail polish")}>Nail polish</li>
           </ul>
         </div>
-        <div className={styles.rightContainer}></div>
+        <div className={styles.rightContainer}>
+          <h2>{title}</h2>
+        </div>
       </div>
     );
   };
