c92d85e9606663f73aafb2b447e55e873ea67cfd
Ana- Becerril <ana.becbel@gmail.com>
2021-09-13 22:19:05 -0500

    working on shop page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div className={styles.mainContainer}>
       <div className={styles.container}>
         <video autoPlay muted loop className={styles.video}>
-        <source src="https://vod-progressive.akamaized.net/exp=1631345764~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4181%2F23%2F595906523%2F2804939859.mp4~hmac=a31cd490e767698cea03620be1280699b6fde05d21bec49aebf4ca8359f22580/vimeo-prod-skyfire-std-us/01/4181/23/595906523/2804939859.mp4?filename=pexels-thirdman-9401750.mp4" type="video/mp4"/>
+        <source src="https://vod-progressive.akamaized.net/exp=1631603794~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4181%2F23%2F595906523%2F2804939859.mp4~hmac=c4344053ece5afe3befcb5a61f038dfd153d15abb23ca2d8b510a6ccb0c74f81/vimeo-prod-skyfire-std-us/01/4181/23/595906523/2804939859.mp4?filename=pexels-thirdman-9401750.mp4" type="video/mp4"/>
         </video>
         <div className={styles.txtContainer}>
         <h1>WE LOVE <span>MAKEUP</span> </h1>
