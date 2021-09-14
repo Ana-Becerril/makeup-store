de935caa9ed985898adb66e28176e313214192f5
Ana- Becerril <ana.becbel@gmail.com>
2021-09-14 12:28:27 -0500

    working on shop page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './style.module.css';
 
-
 const Home = () => {
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.container}>
--- src/components/Shop/index.js
+++ src/components/Shop/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import styles from './style.module.css';
-
-
-
+import {useState, useEffect} from 'react';
+import axios from "axios";
 const Shop = () => {
+
+  const baseURL = "http://makeup-api.herokuapp.com/api/v1/products.json"
+
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+
     return (
       <div className={styles.mainContainer}>
         <div className={styles.leftContainer}>
-          <h1>CATEGORIES</h1>
+          <h2>CATEGORIES</h2>
           <ul>
-            <li>Lashes</li>
-            <li>Eyes</li>
-            <li>Lips</li>
+            <li>Blush</li>
+            <li>Bronzer</li>
+            <li>Eyebrow</li>
+            <li>Eyeliner</li>
+            <li>Eyeshadow</li>
+            <li>Foundation</li>
+            <li>Lip liner</li>
+            <li>Lipstick</li>
+            <li>Mascara</li>
+            <li>Nail polish</li>
           </ul>
         </div>
         <div className={styles.rightContainer}></div>
