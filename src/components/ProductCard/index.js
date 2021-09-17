2286dd1591bb9cea314ee6652a32bf8d08247aad
Ana- Becerril <ana.becbel@gmail.com>
2021-09-17 17:19:49 -0500

    fixed tha search bar and take images from api

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,9 +1,10 @@
 import styles from './style.module.css' 
 
-const ProductCard = () => {
+const ProductCard = ({name, image}) => {
     return (
       <div className={styles.productCardContainer}>
-        <h1>Im a Product Card</h1>
+        <li className={styles.name}> {name} </li>
+        <img src={image}/>
       </div>
     );
   };
