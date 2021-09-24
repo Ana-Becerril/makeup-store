94336cf3239c8ec62ade70ff3effd8ef858120a8
Ana- Becerril <ana.becbel@gmail.com>
2021-09-24 16:29:16 -0500

    working on responsive design

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,12 +8,16 @@ const Home = () => {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.container}>
+        <div className={styles.top}>
         <video autoPlay muted loop src={Video} className={styles.video} />
+        </div>
+        <div className={styles.bottom}>
         <div className={styles.txtContainer}>
           <h1>WE LOVE <span>MAKEUP</span> </h1>
           <div className={styles.button}>
             <NavLink className={styles.cta} to="/shop">SHOP</NavLink>
           </div>
+        </div>
         </div>
       </div>
     </div>
