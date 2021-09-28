a6441dbb668eaa687b6e9e9c9cb51f373c1ffb15
Ana- Becerril <ana.becbel@gmail.com>
2021-09-27 19:16:14 -0500

    fixed overflow on the home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,25 +2,32 @@ import React from 'react';
 import styles from './style.module.css';
 import { NavLink } from 'react-router-dom';
 import Video from '../../assets/home.mp4';
+import {Helmet} from "react-helmet";
+
 
 const Home = () => {
 
   return (
+    <>
+    <Helmet>
+    <style>{'body { background-color: #FCA3B9; }'}</style>
+    </Helmet>
     <div className={styles.mainContainer}>
-      <div className={styles.container}>
         <div className={styles.top}>
         <video autoPlay muted loop src={Video} className={styles.video} />
         </div>
         <div className={styles.bottom}>
         <div className={styles.txtContainer}>
           <h1>WE LOVE <span>MAKEUP</span> </h1>
+          <div className={styles.btnContainer}>
           <div className={styles.button}>
             <NavLink className={styles.cta} to="/shop">SHOP</NavLink>
           </div>
-        </div>
+          </div>
         </div>
       </div>
     </div>
+    </>
   );
 }
 export default Home;
