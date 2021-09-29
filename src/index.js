d80afdddecc993bcc41eeee3118c8d219956eb02
Ana- Becerril <ana.becbel@gmail.com>
2021-09-29 15:51:01 -0500

    added link to use devtools chrome extensions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './Routes'
-import './index.css'
+import {Provider} from 'react-redux';
+import Routes from './Routes';
+import './index.css';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store= {store}>
     <Routes />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,15 +1,15 @@
 import { ActionTypes } from '../constants/action-types';
 
 const initialState = {
-    products: [{
-        id:product.id,
-        title:product.name,
-        price: product.name,
-        image: product.image
-    }],
+    products: [
+    {
+        id:1,
+        title:"name",
+    },
+  ],
 };
 
-export const productReducer = (state, {type, payload}) => {
+export const productReducer = (state=initialState, {type, payload}) => {
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
             return state;
