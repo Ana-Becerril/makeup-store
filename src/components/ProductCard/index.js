0755f4e925577206e7cd57d5db64021dcc7b1f28
Ana- Becerril <ana.becbel@gmail.com>
2021-11-10 12:57:24 -0600

    styled buttons in the cart list using setTimeOut

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,13 +1,25 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './style.module.css'
 import { connect } from 'react-redux';
 import { addToCart, incrementCounter } from '../../redux/actions/actions';
 
 const ProductCard = ({ name, image, price, showDetail, itemFilter, currentCounter, updateCounter, updateCart,id}) => {
 
+  const [titleButton, setTitleButton] = useState("ADD TO CART");
+
+
   const button = useRef();
   const onButtonClick = () => {
-  button.current.style.backgroundColor = "red";
+  button.current.style.backgroundColor = "#FCA3B9";
+  button.current.style.boxShadow= "0 0 0 white";
+  button.current.style.margin= "6px 10px 2px 10px";
+  setTitleButton("ADDED!")
+  setTimeout(() => {
+      button.current.style.backgroundColor = "#6184C6";
+      button.current.style.boxShadow= "2px 1px 2px gray";
+      button.current.style.margin= "4px 10px 4px 10px";
+        setTitleButton("ADD TO CART")
+  }, 700);
   };
 
     return (
@@ -21,7 +33,7 @@ const ProductCard = ({ name, image, price, showDetail, itemFilter, currentCounte
           <button 
           onClick={function(event){ updateCounter(currentCounter); updateCart(id); onButtonClick()}} 
           className={styles.btnModalProductCard}
-          ref={button}> ADD TO CART </button>
+          ref={button}> {titleButton} </button>
           </div>
       </div>
     );
