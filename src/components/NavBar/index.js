b0560d8a88808b47823d538cf1709e3818a165fa
Ana- Becerril <ana.becbel@gmail.com>
2021-11-17 14:40:47 -0600

    clean code

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styles from './style.module.css';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 import Counter from '../Counter';
 import CartModal from '../CartModal';
 
@@ -12,37 +12,6 @@ const NavBar = () => {
     const removeCart = () => setCart(false);
 
     return (
-
-        //         <div className={styles.navContainer} >
-        //             <div className={styles.bar}>
-        //                 <div className={styles.menu}>
-        //                     <div className={`${styles.home} ${styles.coolLink}`}>
-        //                         <NavLink exact to="/">HOME </NavLink>
-        //                     </div>
-        //                     <div className={`${styles.shop} ${styles.coolLink}`}>
-        //                         <NavLink to="/shop">SHOP </NavLink>
-        //                     </div>
-        //                     <div className={`${styles.contact} ${styles.coolLink}`}>
-        //                         <NavLink to="/contact">CONTACT </NavLink>
-        //                     </div>
-        //                 </div>
-        //                 <div className={styles.hamburger}>
-        //                     <FontAwesomeIcon icon={faBars} className={styles.barsIcon} />
-        //                 </div>
-        //             </div>
-        //             <div className={styles.cartContainer}>
-        //                 <Counter />
-        //                 <FontAwesomeIcon onClick={showCart} icon={faShoppingBag} className={styles.cartIcon} />
-        //                 {cart ?
-        //                     <CartModal
-        //                         removeCart={removeCart}
-        //                     /> : null}
-        //             </div>
-        //         </div>
-        //     );
-        // }
-
-
         <div className={styles.navContainer}>
             <input type="checkbox" className={styles.toggler} />
             <div className={styles.hamburger}><div></div></div>
